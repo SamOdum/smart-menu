a00c72f86238e9873ea051e1f34bba8d48793e80
Sam Odum <sam.odum1@gmail.com>
2020-06-19 23:38:35 +0100

    WIP Mobile menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import MainMenu from './components/MainMenu';
+import MobileMenu from './components/MobileMenu';
 
 
 const App = () => {
   return (
+    <>
+    <MobileMenu />
     <MainMenu />
+    </>
   )
 }
 
